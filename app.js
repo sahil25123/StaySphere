e8e6692ee6247122aeda955f03ad2bbb507a3538
sahil25123 <sahilgupta25123@gmail.com>
2025-01-16 13:57:37 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,89 @@
+const express= require('express');
+const app = express();
+const mongoose = require('mongoose');
+const Listing = require('./models/listing');
+const path = require('path');
+const methodOverride = require('method-override');
+const ejsMate= require('ejs-mate');
+
+
+ 
+
+const port = 3001; 
+// Connect to MongoDB
+mongoose.connect('mongodb://localhost:27017/StaySphere')
+main().then(()=> console.log('MongoDB Connected'))
+.catch(err => console.log(err));
+
+async function main() {
+    await mongoose.connect('mongodb://localhost:27017/StaySphere');
+}
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.urlencoded({extended: true}));
+app.use(methodOverride('_method'));
+app.engine('ejs', ejsMate);
+app.use(express.static(path.join(__dirname, 'public')));
+
+ 
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+    }
+)
+
+//index route
+app.get("/listings",  async (req,res) => {
+      const allListing= await Listing.find({})
+      res.render("listings/index.ejs", {listings: allListing})
+    })
+
+app.get("/listings/new", (req,res)=>{
+    res.render("listings/new.ejs")
+        
+})
+
+//create route
+app.post("/listings", async (req, res) => {
+    let {title, description,image, price, location, category}= req.body;
+    const listing= new Listing({title, description, price, location, category});
+    await listing.save();
+    res.redirect(`/listings/${listing._id}`)
+}
+)
+    
+// show route
+app.get("/listings/:id", async (req, res) => {
+    let {id}= req.params;
+    const listing= await Listing.findById(id);
+    res.render("listings/show.ejs", {listing}) 
+})
+
+//edit route
+app.get("/listings/:id/edit", async (req, res) => {
+    let {id}= req.params;
+    const listing= await Listing.findById(id);
+
+    res.render("listings/edit.ejs", {listing})
+
+})
+
+//update route
+app.put("/listings/:id", async (req, res) => {
+    let {id}= req.params;
+    let {title, description, image, price, location, category}= req.body;
+    const listing= await Listing.findByIdAndUpdate
+    (id, {title, description, image, price, location, category}, {new: true});
+    res.redirect(`/listings/${listing._id}`)
+    })
+
+//delete route
+app.delete("/listings/:id", async (req, res) => {
+    let {id}= req.params;
+    await Listing.findByIdAndDelete(id);
+    res.redirect("/listings");
+    })
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+    });
---
+++ models/listing.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+const listingSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true, // 'required' is the correct field name
+    },
+    description: String,
+    price: {
+        type: Number,
+        required: true,  // Adding a required field for price
+    },
+    image: {
+        filename: {
+            type: String,
+            default: 'listingimage', // Default filename if not provided
+        },
+        url: {
+            type: String,
+            default: 'https://images.unsplash.com/photo-1735675376752-2e0fb3de9f69?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', // Default image URL
+            set: (v) => v === "" ? 'https://images.unsplash.com/photo-1735675376752-2e0fb3de9f69?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' : v, // Setter function for empty string
+        },
+    },
+    location: String,
+    country: String,
+});
+
+const Listing = mongoose.model('Listing', listingSchema);
+module.exports = Listing;
