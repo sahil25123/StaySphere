361933f804994ae5140a40484ed2fa86a8c70c48
sahil25123 <sahilgupta25123@gmail.com>
2025-01-19 14:45:13 +0530

    Rename app.js to index.js and update start script in package.json

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express= require('express');
 const app = express();
 const mongoose = require('mongoose');
-const Listing = require('./models/listing');
+const Listing = require('./models/listing.js');
 const path = require('path');
 const methodOverride = require('method-override');
 const ejsMate= require('ejs-mate');
